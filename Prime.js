29db2ac73c1f8a2b3f5a8bb99f64e4e2910ef8d4
Raoul Grosser <RaoulCoder@outlook.com>
2013-06-20 21:51:01 +0300

    Create Prime.js

---
+++ Prime.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env prime
+(function (sys) {
+        var i = 2,
+            isPrime = function () {
+                    var j = 2
+                        max = i / 2;
+                    while ( j <= max ) {
+                        if ( i % j === 0 ) {
+                                return false;
+                        }
+                        j += 1;
+                    }
+                    return true;
+            },
+            result = [];
+ 
+        while ( i < 10000 ) {
+                if ( isPrime() ) {
+                        result.push(i);
+                }
+                i += 1;
+        }
+ 
+        sys.puts(result);
+ 
+}(require("sys")));
